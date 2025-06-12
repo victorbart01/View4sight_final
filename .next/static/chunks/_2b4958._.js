a82906e25abcd0cc288068cf5a9e7c7f5620b5ca
Mewcode <169058147+victorbart01@users.noreply.github.com>
2025-06-12 17:21:16 +0700

    v0.8

--- .next/static/chunks/_2b4958._.js
+++ .next/static/chunks/_2b4958._.js
@@ -85,7 +85,7 @@ function Hero() {
                                     className: "panel text-center overflow-hidden rounded-3 border border-5 border-dark dark:border-white dark:border-opacity-20",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                                         alt: "View4Sight collaboration dashboard",
-                                        src: "/assets/images/template/dashboard.jpg",
+                                        src: "/assets/images/template/home-06-builder-tools.png",
                                         width: "1440",
                                         height: "1024"
                                     }, void 0, false, {
